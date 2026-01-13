d8d77256ef9cc004f37a3a8e6c51bc5d541e4067
Jakub Pilarski <84476679+jpilarski@users.noreply.github.com>
2026-01-13 19:00:07 +0100

    Add files via upload

---
+++ Pilarski.PlayerManager.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Pilarski.PlayerManager.Web.Models;
+using System.Diagnostics;
+
+namespace Pilarski.PlayerManager.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Pilarski.PlayerManager.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pilarski.PlayerManager.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
